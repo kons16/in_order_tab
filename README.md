d6e7360000f52100997e755bca02b7fcd92f35f3
kons16 <ks1216r@gmail.com>
2021-03-12 20:54:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# In Order Tab
+
+In Order Tab は適当に開かれた複数のタブを ABC Order に並び替える chrome 拡張機能です.  
+ただし、Youtube のリンクはタブの先頭にくるようになっています.
---
+++ background.js
@@ -0,0 +1,40 @@
+chrome.contextMenus.create({title: "In Order Tab", onclick: inOrderTab}, function(){
+});
+
+
+function inOrderTab() {
+  chrome.tabs.query({}, tabs => {
+    let urls = [];
+    let tabids = [];
+    
+    for(let i = 0; i < tabs.length; i++) {
+      urls.push(tabs[i].url);
+      tabids.push(tabs[i].id);
+    }
+
+    urls.sort();
+
+    // youtubeは先頭にする
+    let youtube_index = [];
+    for(let j = 0; j < urls.length; j++) {
+      if(urls[j].indexOf("youtube") != -1) {
+        youtube_index.push(j);
+      }
+    }
+    youtube_index.forEach(index => {
+      urls.unshift(urls[index]);
+      urls.splice(index+1, 1);
+    });
+    
+    // 現在開いているタブを削除
+    for(let j = 0; j < tabids.length; j++) {
+      chrome.tabs.remove(tabids[j]);
+    }
+
+    // タブの並び替え
+    for(let j = 0; j < urls.length; j++) {
+      chrome.tabs.create({url: urls[j]});
+    }
+  });
+};
+
